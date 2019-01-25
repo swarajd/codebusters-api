6bb0c09c1e466b8037ccd4921d83ba4ca6a590ca
swarajd <swaraj.dhumne@gmail.com>
2019-01-25 11:36:35 -0500

    added jest, and prettier

--- src/app.ts
+++ src/app.ts
@@ -1,13 +1,13 @@
-import express = require('express');
+import express = require("express");
 
 const app: express.Application = express();
 
-app.get('/', function(req, res) {
-    res.send("Hello world!");
-})
+app.get("/", function(req, res) {
+  res.send("Hello world!");
+});
 
 let port: number = Number(process.env.PORT) || 3000;
 
 app.listen(port, function() {
-    console.log(`example app listening on port ${port}`);
-})
+  console.log(`example app listening on port ${port}`);
+});
